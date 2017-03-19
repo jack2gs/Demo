48aceef7241df4f6d9827531983c36d753bfd892
jack2gs <sailor.g@163.com>
2017-03-19 16:48:24 +0800

    finished - use browserify to bundle - use sourcemaps to build the source map - use uglify to compress the file - use watchify to trigger the automated building process - use browser-sync to auto refresh the browser

--- typescript-ci/gulpfile.js
+++ typescript-ci/gulpfile.js
@@ -1,26 +1,51 @@
 var gulp = require("gulp")
     browserify = require("browserify"),
     source = require("vinyl-source-stream"),
+    watchify = require("watchify"),
     tsify = require("tsify"),
+    gutil = require("gulp-util"),
+    uglify = require("gulp-uglify"),
+    sourcemaps = require("gulp-sourcemaps"),
+    buffer = require("vinyl-buffer"),
+    browserSync = require('browser-sync').create(),
     paths = {
         pages: ["src/*.html"]
     };
+
+var watchedBrowserify = watchify(browserify({
+        basedir: ".",
+        debug: true,
+        entries: ["src/main.ts"],
+        cache: {},
+        packageCatche: {}
+}).plugin(tsify));
 
 gulp.task("copy-html", function(){
     return gulp.src(paths.pages)
         .pipe(gulp.dest("dist"));
 });
 
-gulp.task("default", ["copy-html"], function(){
-    return browserify({
-        basedir: ".",
-        debug: true,
-        entries: ["src/main.ts"],
-        cache: {},
-        packageCatche: {}
-    })
-    .plugin(tsify)
-    .bundle()
-    .pipe(source("bundle.js"))
-    .pipe(gulp.dest("dist"));
+function bundle(){
+    return watchedBrowserify
+        .bundle()
+        .pipe(source("bundle.js"))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest("dist")); 
+}
+
+gulp.task("default", ["copy-html"], bundle);
+
+gulp.task("browser-sync", function(){
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+    gulp.watch("./dist/**/*.*").on("change", browserSync.reload);
 });
+
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", gutil.log);
--- typescript-ci/src/greet.ts
+++ typescript-ci/src/greet.ts
@@ -1,3 +1,5 @@
 export function sayHello(name: string){
+    console.log("name: " + name);
+    console.log("Hello world from Jack");
     return `Hello from ${name}`;
 }
